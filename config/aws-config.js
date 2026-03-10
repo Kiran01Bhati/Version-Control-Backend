c23cafdbbf0f9610ba6269f6effcd5e16a8e3a65
Kiran01Bhati <nehabhati20033@gmail.com>
2026-03-10 21:58:02 +0530

    implemmented secure AWS S3 push logic using dotenv

--- config/aws-config.js
+++ config/aws-config.js
@@ -1,11 +1,17 @@
 // code of aws connection code like connect with account/bucket
-
+require("dotenv").config();
 const AWS = require("aws-sdk");
 
+
 // Set the AWS region and credentials according to target audiencenot not to develeopment
-AWS.config.update({ region: "ap-south-1" });
+AWS.config.update({ 
+    
+   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+   region:process.env.AWS_REGION
+});
 
 const s3 = new AWS.S3();
-const S3_BUCKET = "testsamplebucketkiran";
+const S3_BUCKET = process.env.S3_BUCKET;
 
 module.exports = { s3, S3_BUCKET };
--- controllers/push.js
+++ controllers/push.js
@@ -1,18 +1,50 @@
 const fs = require('fs').promises;
 const path  = require('path');
-const {s3, s3_Bucket } = require("../config/aws-config");
+const {s3, s3_Bucket, S3_BUCKET } = require("../config/aws-config");
 
 
 async function pushRepo(){
  //read file from current working directory
- const repoPath = path.resolve(process.cwd(), "apnaGit");
+ const repoPath = path.resolve(process.cwd(), ".apnaGit");
 
  //commits path = commits folder inside repoPath
  const commitsPath = path.join(repoPath, "commits");
 
+
 try{
+    //jitni files hain commits folder mein, unko read karenge and push karenge
+    const commitDirs  = await fs.readdir(commitsPath);
+
+  //outer loop to read each commit directory
+  for(const commitDir of commitDirs){
+const commitPath  = path.join(commitsPath, commitDir);
+const files = await fs.readdir(commitPath);
+//inner loop to read each file inside commit directory
+for(const file of files) {
+const filePath = path.join(commitPath, file);
+
+//read file content
+const fileContent  = await fs.readFile(filePath);
+
+//actaul properties for uploading to AWS 
+const params = {
+  Bucket: S3_BUCKET,
+
+  //folder structure in s3 will be commits/commitDir/file
+  Key : `commits/${commitDir}/${file}`,
+  Body : fileContent,
+
+};
+await s3.upload(params).promise();
+
+}
+}
+
+console.log("All Commits pushed to s3 Successfully!");
+
 
 }catch(err){
+  //we will log the error if any error occurs
     console.error("Error pushing to s3:", err);
 }
 
