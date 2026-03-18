f0d9fcceda12ef49daaa0cb0366a6d4e95d6de72
Kiran01Bhati <nehabhati20033@gmail.com>
2026-03-18 23:59:36 +0530

    SignUp functionality implmented

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,54 @@
+const JWT = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const {MongoClient} = require('mongodb');
+const dotenv = require('dotenv');
+
+
+dotenv.config();
+const uri = process.env.MONGODB_URL;
+let client;
+
+async function connectClient(){
+    if(!client){
+        client = new MongoClient(uri, 
+            {userNewUrlParser:true, 
+            useUnifiedTopology:true,
+            });
+            await client.connect();
+    }
+}
+
 const  getAllUsers = (req,res) => {
-    res.send("All Users fetched!");
+   
 };
 
 const signUp = (req, res) => {
-    res.send("user signed up!");
+    const {username, password, email } = req.body;
+    try{
+        await connectClient();
+        const db = client.db("githubclone");
+        const usersCollection = db.collection("users");
+
+        const user = await usersCollection.findOne({username});
+
+        if(user){
+            return res.status(400).json({message:"User already exists!"});
+        }
+       const salt = await bcrypt.genSalt(10);
+       const hashedPassword = await bcrypt.hash(password, salt);
+        
+       const newUser = {
+        username, 
+        password:hashedPassword,
+        email,
+        repositories : [],
+        followedUsers :[],
+        startRepos : []
+      }
+      const result = await usersCollection.insertOne(newUser);
+
+      const token = JWT.sign({id:result.insertId})
+    }
 };
 
 const login = (req, res) => {
@@ -35,7 +80,15 @@ module.exports = {
 
 /*
 DB se data fetch karega
-
 Validation karega
+Business logic handle karega
 
-Business logic handle karega*/
+AUTHENTICATION:-
+
+jsonwebtoken → JWT authentication ke liye
+bcryptjs → password hashing ke liye
+mongodb → database connect karne ke liye
+
+
+
+*/
